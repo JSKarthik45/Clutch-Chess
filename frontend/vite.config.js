6b5eea580fdc43c20b3569fc29b362e8697362ea
Karthik <jskarthik45@gmail.com>
2025-09-15 17:36:13 +0530

    v1.47

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'images/ClutChess.svg',
+      ],
+      manifest: {
+        name: 'CLUTCHESS',
+        short_name: 'Clutchess',
+        description: 'Play, Practice And Improve Your Chess Skills',
+        theme_color: '#000000ff',
+        icons: [
+          {
+            src: 'images/ClutChess.svg',  // path relative to `public/`
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true // enables PWA during development
+      }
+    })
   ],
   server : {
     port: 3000, 
